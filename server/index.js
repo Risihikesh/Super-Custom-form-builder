500a5404e0372319265fdbc07efb317926b89427
Rishikesh <krishikesh369@gmail.com>
2023-11-03 01:20:15 +0530

    gh

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
+
 const mongoose = require("mongoose");
 app.use(express.json({ limit: "50mb" }));
 var cors = require("cors");
